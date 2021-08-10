b22125b829856a74d20c3c1c4a1b1c239b43fbd1
2bleO <Onesis.olea@gmail.com>
2021-08-10 18:33:24 -0400

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,16 +3,17 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: {
-    index: './src/index.js',
-  },
+  entry: './src/index.js',
   devtool: 'inline-source-map',
   devServer: {
     contentBase: './dist',
   },
+  watchOptions: {
+    poll: true,
+    ignored: /node_modules/,
+  },
   plugins: [
     new HtmlWebpackPlugin({
-      title: '',
       template: './src/index.html',
     }),
   ],
@@ -20,7 +21,6 @@ module.exports = {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
-    publicPath: '/',
   },
   module: {
     rules: [
