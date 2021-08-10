64e61942c6817af26c4c323d1452554a0e1a5a55
bishoy-samwel <bishoy-samwuel@gmail.com>
2021-08-10 17:52:17 +0200

    Initial api setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import './style.css';
+
+const Pokedex = require('pokeapi-js-wrapper');
+
+const P = new Pokedex.Pokedex();
+
+let list;
+
+(async () => {
+  list = await P.getPokedexsList();
+  console.log(list.results[3].name);
+})();
