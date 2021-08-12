5e1cba479e7f0d30d8206f13c8ceb03ad58da886
bishoy-samwel <bishoy-samwuel@gmail.com>
2021-08-12 14:44:20 +0200

    Add manageEvents functions to handle comments

--- src/comment.js
+++ src/comment.js
@@ -9,31 +9,36 @@ export default class Comment {
     usernameInput.type = 'text';
     commentInput.type = 'text';
     commentBtn.innerHTML = 'Comment';
-    commentBtn.addEventListener('click', Comment.addComment());
     form.append(usernameInput, commentInput, commentBtn);
     return form;
   }
 
+  static manageEvents() {
+    document.querySelector('button').addEventListener('click', (event) => {
+      event.preventDefault();
+      const inputs = document.querySelectorAll('input[type="text"]');
+      const username = inputs[0].value;
+      const comment = inputs[1].value;
+      const obj = { item_id: '333', username, comment };
+      console.log(obj);
+      Api.postComment(obj);
+    });
+  }
+
   static addComment() {
-    const [inputs] = document.querySelectorAll('input');
-    const username = inputs[0];
-    const comment = inputs[1];
-    const obj = { item_id: '0', username, comment };
-    Api.postComment(obj);
+
   }
 
   static createComment(obj) {
-    console.log(obj);
     const p = document.createElement('p');
     p.innerHTML = `${obj.creation_date} ${obj.username}: ${obj.comment}`;
     return p;
   }
 
-  static async loadComments(item_id = 'item1') {
+  static async loadComments(item_id = '333') {
     const div = document.createElement('div');
     await Api.getComments(item_id);
-    Api.data.forEach((obj) => {
-      console.log(obj);
+    Api.commentsData.forEach((obj) => {
       div.append(Comment.createComment(obj));
     });
     return div;
