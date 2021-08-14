26a6a3f5c428e38794b652ac09029daf3522f903
bishoy-samwel <bishoy-samwuel@gmail.com>
2021-08-14 14:53:43 +0200

    Passing the full info instead of the keys individually

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import './style.css';
 import { layout, render, manageEvents } from './layout';
-import { getlist1, getpokeinfo } from './pokemon-list';
 import Pokemon from './pokemon';
 
 const body = document.getElementById('body');
@@ -9,12 +8,11 @@ body.innerHTML = layout();
 async function pokelist(num) {
 // async function pokelist() {
 // const list = ['bulbasaur', 'ivysaur', 'venusaur', 'charmander', 'charmeleon', 'charizard'];
-  const list = await getlist1(num, 0);
+  const list = await Pokemon.getlist(num, 0);
   list.results.forEach(async (element) => {
   // list.forEach(async (element) => {
-    const info = await getpokeinfo(element.name);
-    const pokeimage = info.sprites.other.dream_world.front_default;
-    const pokemon = new Pokemon(info.id, info.name, 0, pokeimage);
+    const info = await Pokemon.getpokeinfo(element.name);
+    const pokemon = new Pokemon(info);
     render(pokemon);
   });
 }
