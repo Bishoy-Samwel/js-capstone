9bc8affe4f31e1f0dc789c2a4e82eda115509066
2bleO <Onesis.olea@gmail.com>
2021-08-14 02:55:47 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,21 +2,30 @@ import './style.css';
 import { layout, render } from './layout';
 import { getlist1, getpokeinfo } from './pokemon-list';
 import Pokemon from './pokemon';
-
+import { fetchLikes, pokelike, thispokelikes } from './likes';
+// import Api from './api';
 const body = document.getElementById('body');
 body.innerHTML = layout();
 
 async function pokelist(num) {
-// async function pokelist() {
-// const list = ['bulbasaur', 'ivysaur', 'venusaur', 'charmander', 'charmeleon', 'charizard'];
+  // async function pokelist() {
+  // const list = ['bulbasaur', 'ivysaur', 'venusaur', 'charmander', 'charmeleon', 'charizard'];
   const list = await getlist1(num, 0);
+  const likes = await fetchLikes();
+  console.log(likes);
   list.results.forEach(async (element) => {
-  // list.forEach(async (element) => {
     const info = await getpokeinfo(element.name);
+    const thislikes = await thispokelikes(likes, info.id);
     const pokeimage = info.sprites.other.dream_world.front_default;
-    const pokemon = new Pokemon(info.id, info.name, 0, pokeimage);
-    console.log(pokemon);
+    const pokemon = new Pokemon(info.id, info.name, thislikes, pokeimage);
     render(pokemon);
+    const likebtn = document.getElementById(`${pokemon.id}-like`);
+    likebtn.addEventListener('click', async () => {
+      await pokelike(pokemon.id);
+      likebtn.classList.add('liked');
+      window.location.reload();
+    });
   });
 }
-pokelist(100);
+
+pokelist(9);
