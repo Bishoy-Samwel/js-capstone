eb2119ff7d4c073e6f4a168f8a21b6cfcccaf26f
bishoy-samwel <bishoy-samwuel@gmail.com>
2021-08-14 14:54:59 +0200

    Import Pokemon, Change from poke.id to poke.name, Add getDetailsDiv

--- src/layout.js
+++ src/layout.js
@@ -1,5 +1,6 @@
 import Comment from './comment';
 import Modal from './modal';
+import Pokemon from './pokemon';
 
 const layout = () => `
 <header>
@@ -27,8 +28,8 @@ const layout = () => `
 
 function render(pokemon) {
   const ul = document.getElementById('pokelist');
-  ul.appendChild(document.createElement('div')).setAttribute('id', pokemon.id);
-  const poke = document.getElementById(pokemon.id);
+  ul.appendChild(document.createElement('div')).setAttribute('id', pokemon.name);
+  const poke = document.getElementById(pokemon.name);
   poke.classList.add('pokemon');
   poke.innerHTML = `
           <img src="${pokemon.image}" alt="" class="poke-img" width="200px" height="200px">
@@ -52,17 +53,37 @@ const manageEvents = () => {
   };
 };
 
+const getDetailsDiv = async (id) => {
+  const div = document.createElement('div');
+  const info = await Pokemon.getpokeinfo(id);
+  const heightP = document.createElement('p');
+  heightP.innerHTML = `<b>Height:</b> ${await info.height}`;
+  const weightP = document.createElement('p');
+  weightP.innerHTML = `<b>Weight:</b> ${await info.weight}`;
+  const snameP = document.createElement('p');
+  snameP.innerHTML = `<b>Species Name:</b> ${await info.species.name}`;
+  div.append(heightP, weightP, snameP);
+  return div;
+};
+
 const modalContent = async (id, requestType) => {
+  const info = await Pokemon.getpokeinfo(id);
   const modal = new Modal();
+  let commentsP = '';
+  const imgDiv = document.createElement('div');
+  imgDiv.innerHTML = `<img src=${info.sprites.other.dream_world.front_default} alt=""class="poke-img" width="200px" height="200px">`;
   if (requestType === 'comment') {
     let commentsDiv = '';
     try {
-      commentsDiv = await Comment.loadComments(Number(id));
+      commentsDiv = await Comment.loadComments(id);
+      const n = Comment.numOfComments;
+      commentsP = document.createElement('p');
+      commentsP.innerHTML = `<b>Comments: </b> ${n}`;
     } catch (error) {
       console.error(error);
     }
     const form = Comment.generateForm(id);
-    modal.create([form, commentsDiv]);
+    modal.create([commentsP, imgDiv, await getDetailsDiv(id), form, commentsDiv]);
     modal.pop(modal.boxDiv);
   } else if (requestType === 'reseve') {
     //
