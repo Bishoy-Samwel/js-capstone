89530176271e6e8a8d2b6857d552d646d23e9e2c
2bleO <Onesis.olea@gmail.com>
2021-08-14 14:06:06 -0400

    Update pokecounter.js

--- src/pokecounter.js
+++ src/pokecounter.js
@@ -1,11 +1,7 @@
-const displayedpokemons = async () => {
+const displaycounter = async () => {
   const pokemons = document.querySelectorAll('.pokemon').length;
-  return pokemons;
-};
-
-const displaycounter = async (num) => {
   const pokenav = document.getElementById('pokenav');
-  pokenav.innerHTML = `<span>Pokemons (${num})</span>`;
+  pokenav.innerHTML = `<span>Pokemons (${pokemons})</span>`;
 };
 
-export { displayedpokemons, displaycounter };
+export default displaycounter;
